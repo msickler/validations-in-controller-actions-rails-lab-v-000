43c3520afde19ccb061d3d3fe1d768464eb49861
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-12 01:36:07 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,7 @@
 class AuthorsController < ApplicationController
   validates :name, presence: true
   validates :email, uniqueness: true
-  
+
   def show
     @author = Author.find(params[:id])
   end
